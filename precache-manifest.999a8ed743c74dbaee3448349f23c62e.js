d065b44005852a88dc2c3ea815690bb9ffcb3f63
Alexander Saavedra <mralexsaavedra@gmail.com>
2020-09-23 19:49:30 +0200

    Updates

---
+++ precache-manifest.999a8ed743c74dbaee3448349f23c62e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12cd79ec3fb7f3f134c4bc95a19f6b7",
+    "url": "/prueba-fintonic/index.html"
+  },
+  {
+    "revision": "349f5fb50fa4f7409dc0",
+    "url": "/prueba-fintonic/static/css/main.40ee76cf.chunk.css"
+  },
+  {
+    "revision": "4b7649ff871422d88f23",
+    "url": "/prueba-fintonic/static/js/2.25df8c2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prueba-fintonic/static/js/2.25df8c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "349f5fb50fa4f7409dc0",
+    "url": "/prueba-fintonic/static/js/main.d1b3fb36.chunk.js"
+  },
+  {
+    "revision": "dedd3a6c6d56d5a75cc0",
+    "url": "/prueba-fintonic/static/js/runtime-main.11d57d6d.js"
+  }
+]);
